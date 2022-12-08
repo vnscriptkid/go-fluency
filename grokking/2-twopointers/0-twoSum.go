64982c880b89d7a803918fddfa5277dad73acac2
Thanh Nguyen <vnscriptkid@gmail.com>
2022-12-08 20:16:19 +0800

    feat(grokking): two sum

---
+++ grokking/2-twopointers/0-twoSum.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+// Given an array of sorted numbers and a target sum,
+// find a pair in the array whose sum is equal to the given target.
+
+// 1  2  3  4  6
+//    ^
+//          $
+
+// sum: 6 => (1,3)
+// target: 6
+
+func twoSum(arr []int, targetSum int) (int, int) {
+
+	left := 0
+	right := len(arr) - 1
+	var curSum int
+
+	for left < right {
+		curSum = arr[left] + arr[right]
+
+		if curSum == targetSum {
+			return left, right
+		}
+
+		if curSum < targetSum {
+			left++
+		} else {
+			right--
+		}
+	}
+
+	return -1, -1
+}
+
+func main() {
+	fmt.Println(twoSum([]int{1, 2, 3, 4, 6}, 6))
+}
