90a5153da2ca28e23dedf0a107d52b4f497f15d4
Thanh Nguyen <vnscriptkid@gmail.com>
2022-12-09 18:34:09 +0800

    feat(grokking): cyclic sort

---
+++ grokking/5-cyclic-sort/1-cyclic-sort.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func cyclicSort(arr []int) {
+	// [3, 1, 5, 4, 2]
+	i := 0
+
+	for i < len(arr) {
+		correctIdx := arr[i] - 1
+
+		if i == correctIdx {
+			i++
+			continue
+		}
+
+		arr[i], arr[correctIdx] = arr[correctIdx], arr[i]
+	}
+}
+
+func main() {
+	// Input: [3, 1, 5, 4, 2]
+	// Output: [1, 2, 3, 4, 5]
+	arr := []int{3, 1, 5, 4, 2}
+	cyclicSort(arr)
+	fmt.Println(arr)
+
+	// 	Input: [2, 6, 4, 3, 1, 5]
+	// Output: [1, 2, 3, 4, 5, 6]
+
+	arr = []int{2, 6, 4, 3, 1, 5}
+	cyclicSort(arr)
+	fmt.Println(arr)
+}
