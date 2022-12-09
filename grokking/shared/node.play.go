4840d86ff21a5f42031a5b4f42b646c4546e9405
Thanh Nguyen <vnscriptkid@gmail.com>
2022-12-09 19:45:07 +0800

    feat(grokking): update shared

--- grokking/shared/node.play.go
+++ grokking/shared/node.play.go
@@ -1,4 +1,4 @@
-package shared
+package main
 
 import (
 	"fmt"
@@ -26,4 +26,14 @@ func (n *Node) GetPrinted() string {
 
 func (n *Node) Print() {
 	fmt.Println(n.GetPrinted())
+}
+
+func main() {
+	node10 := Node{Value: 10}
+	node8 := Node{Value: 8, Next: &node10}
+	node6 := Node{Value: 6, Next: &node8}
+	node4 := Node{Value: 4, Next: &node6}
+	node2 := Node{Value: 2, Next: &node4}
+
+	node2.Print()
 }
