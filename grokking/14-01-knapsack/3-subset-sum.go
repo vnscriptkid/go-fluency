cce5fd12a9f36429bef4476c180765aeff917bb6
Thanh Nguyen <vnscriptkid@gmail.com>
2023-01-19 20:34:39 +0800

    feat: subset sum bottom up

--- grokking/14-01-knapsack/3-subset-sum.go
+++ grokking/14-01-knapsack/3-subset-sum.go
@@ -27,3 +27,43 @@ func subsetSumTopdown(subset []int, s int) bool {
 
 	return recur(subset, 0, s, map[string]bool{})
 }
+
+func subsetSumBottomUp(subset []int, s int) bool {
+	//    0  1  2  3  4  5  6
+	// 1  x  x  _  _  _  _  _
+	// 2  x  x  x  x  _  _  _
+	// 3  x  x  x  x  x  x  x
+	// 7  x
+
+	dp := make([][]bool, len(subset))
+
+	for i := range dp {
+		dp[i] = make([]bool, s+1)
+	}
+
+	for i := 0; i < len(subset); i++ {
+		for j := 0; j <= s; j++ {
+			if j == 0 || j == subset[i] {
+				dp[i][j] = true
+				continue
+			}
+
+			dp[i][j] = false
+		}
+	}
+
+	for i := 1; i < len(subset); i++ {
+		for j := 1; j <= s; j++ {
+			if dp[i-1][j] == true {
+				dp[i][j] = true
+				continue
+			}
+
+			if j >= subset[i] {
+				dp[i][j] = dp[i-1][j-subset[i]]
+			}
+		}
+	}
+
+	return dp[len(subset)-1][s]
+}
