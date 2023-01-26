cfeab086066cea11e82a3502fa5a5ac840181d12
Thanh Nguyen <vnscriptkid@gmail.com>
2023-01-26 15:59:19 +0800

    feat: more tests for waitgroup

--- concurrency/waitgroups_submit/main_test.go
+++ concurrency/waitgroups_submit/main_test.go
@@ -4,6 +4,7 @@ import (
 	"io"
 	"os"
 	"strings"
+	"sync"
 	"testing"
 )
 
@@ -41,4 +42,58 @@ func Test_printSth(t *testing.T) {
 	if !strings.Contains(actualOutput, "example") {
 		t.Errorf("print wrong thing")
 	}
+}
+
+func Test_updateString(t *testing.T) {
+	var wg sync.WaitGroup
+
+	str = "old stuff"
+
+	wg.Add(1)
+
+	newStr := "new stuff"
+
+	updateString(newStr, &wg)
+
+	wg.Wait()
+
+	if str != newStr {
+		t.Errorf("Expected %s but got %s", newStr, str)
+	}
+}
+
+func Test_main(t *testing.T) {
+	stdout := os.Stdout
+
+	r, w, _ := os.Pipe()
+
+	os.Stdout = w
+
+	main()
+
+	w.Close()
+
+	bSlice, err := io.ReadAll(r)
+
+	if err != nil {
+		t.Errorf("err reading r pipe")
+	}
+
+	output := string(bSlice)
+
+	items := strings.Split(output, "\n")
+
+	if !strings.Contains(items[0], "one") {
+		t.Errorf("Expected %s but got %s", "one", items[0])
+	}
+
+	if !strings.Contains(items[1], "two") {
+		t.Errorf("Expected %s but got %s", "two", items[1])
+	}
+
+	if !strings.Contains(items[2], "three") {
+		t.Errorf("Expected %s but got %s", "three", items[2])
+	}
+
+	os.Stdout = stdout
 }
