818135a83e668e98b789300aa174d3cb4be9979b
Thanh Nguyen <vnscriptkid@gmail.com>
2023-01-31 23:00:51 +0800

    feat: min subset sum diff bottom up

--- grokking/14-01-knapsack/4-min-subset-sum-diff.go
+++ grokking/14-01-knapsack/4-min-subset-sum-diff.go
@@ -44,3 +44,63 @@ func minSubsetSumDiff(subset []int) int {
 	return minSubsetSumDiffRecur(subset, 0, 0, &globalMin, total, map[string]int{})
 
 }
+
+func minSubsetSumDiffBottomUp(subset []int) int {
+	total := 0
+
+	for _, x := range subset {
+		total += x
+	}
+
+	half := int(math.Ceil(float64(total) / 2))
+
+	var dp [][]bool
+
+	// 1,2,3,9 => 15/2 ~ 8
+
+	//    0  1  2  3  4  5  6  7  8
+	// 1  x  x  _  _  _  _  _  _  _
+	// 2  x
+	// 3  x
+	// 9  x
+
+	for i := 0; i < len(subset); i++ {
+		dp = make([][]bool, len(subset))
+	}
+
+	for i := range dp {
+		dp[i] = make([]bool, half+1)
+	}
+
+	for i := range dp {
+		dp[i][0] = true
+	}
+
+	if subset[0] <= half {
+		dp[0][subset[0]] = true
+	}
+
+	for i := 1; i < len(subset); i++ {
+		for j := 1; j <= half; j++ {
+			if dp[i-1][j] {
+				dp[i][j] = true
+				continue
+			}
+
+			if j >= subset[i] {
+				dp[i][j] = dp[i-1][j-subset[i]]
+			}
+		}
+	}
+
+	for j := half; j >= 0; j-- {
+		if dp[len(subset)-1][j] {
+			other := total - j
+			diff := int(math.Abs(float64(other - j)))
+
+			return diff
+		}
+	}
+
+	return -1
+}
