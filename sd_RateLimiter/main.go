2c211ecec03b32620c4eb81918709967bdaaff8e
Thanh Nguyen <vnscriptkid@gmail.com>
2023-07-10 00:51:29 +0800

    feat(sd/RateLimiter): Sliding window strategy

--- sd_RateLimiter/main.go
+++ sd_RateLimiter/main.go
@@ -9,10 +9,19 @@ import (
 	"github.com/go-redis/redis"
 )
 
+type RateLimitStrategy string
+
+var RateLimitStrategy_fixedWindow RateLimitStrategy = "fixed-window"
+var RateLimitStrategy_slidingWindow RateLimitStrategy = "sliding-window"
+
 var redisClient *redis.Client
 
 // define the rate limiting middleware
-func rateLimiter(c *gin.Context) {
+// https://redislabs.com/redis-best-practices/basic-rate-limiting/
+// Fixed Window Counter: 10 requests / 5 seconds
+// Pros: Simple, easy to implement (user_id: count)
+// Cons: thundering herd problem, happens between 2 windows (in short period of time, huge amount of requests may be sent and still be accepted)
+func rateLimiter_fixedWindow(c *gin.Context) {
 	// get the user ID from the x-user-id header
 	userID := c.GetHeader("x-user-id")
 
@@ -39,15 +48,13 @@ func rateLimiter(c *gin.Context) {
 	}
 
 	if count == 0 {
-		// set the initial count to 1
-		err := redisClient.Set(key, 1, 5*time.Second).Err()
+		// set the initial count to 0
+		err := redisClient.Set(key, 0, 5*time.Second).Err()
+
 		if err != nil {
 			c.AbortWithError(500, err)
 			return
 		}
-
-		c.Next()
-		return
 	}
 
 	// increment the count
@@ -79,7 +86,7 @@ func main() {
 	}
 
 	// use the rate limiting middleware on all routes
-	r.Use(rateLimiter)
+	r.Use(NewRateLimiter(RateLimitStrategy_slidingWindow))
 
 	// define a test route
 	r.GET("/", func(c *gin.Context) {
@@ -90,4 +97,70 @@ func main() {
 
 	// start the server
 	r.Run(":8080")
+}
+
+func NewRateLimiter(strategy RateLimitStrategy) func(c *gin.Context) {
+	switch strategy {
+	case "fixed-window":
+		return rateLimiter_fixedWindow
+	case "sliding-window":
+		return rateLimiter_slidingWindow
+	default:
+		panic("invalid rate limiting strategy")
+	}
+}
+
+// Pros: Solve the thundering herd problem
+// Cons: footprint is bigger (user_id: timestamp)
+func rateLimiter_slidingWindow(c *gin.Context) {
+	// get the user ID from the x-user-id header
+	userID := c.GetHeader("x-user-id")
+
+	if userID == "" {
+		c.AbortWithStatus(400) // 400 Bad Request
+	}
+
+	const Limit = 10
+	const Window = time.Second * 5
+
+	// set the key and limit based on the user ID
+	key := fmt.Sprintf("rate_limiter:%s", userID)
+
+	now := time.Now().UnixNano()
+	windowStart := now - int64(Window)
+
+	fmt.Printf("now: %v\n, windowStart: %v\n", now, windowStart)
+
+	// Clean up old requests from the sliding window.
+	_, err := redisClient.ZRemRangeByScore(key, "-inf", fmt.Sprintf("%v", windowStart)).Result()
+	if err != nil {
+		c.AbortWithError(500, err)
+		return
+	}
+
+	// Count the number of requests in the sliding window.
+	count, err := redisClient.ZCount(key, fmt.Sprintf("%v", windowStart), "+inf").Result()
+	c.Writer.Header().Set("X-RateLimit-Limit", fmt.Sprintf("%v/%v", count, Limit))
+	if err != nil {
+		c.AbortWithError(500, err)
+		return
+	}
+
+	// Check if the rate limit has been reached.
+	if count >= Limit {
+		c.AbortWithStatus(429) // 429 Too Many Requests
+		return
+	}
+
+	// Add the new request to the sliding window.
+	_, err = redisClient.ZAdd(key, redis.Z{Score: float64(now), Member: now}).Result()
+	if err != nil {
+		c.AbortWithError(500, err)
+		return
+	}
+
+	// Automatically clean up old requests from the sliding window by setting an expiry time.
+	_ = redisClient.Expire(key, Window)
+
+	c.Next()
 }
