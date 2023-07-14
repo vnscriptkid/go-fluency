bd26ecda123c62e75931737aebbae492ca606021
Thanh Nguyen <vnscriptkid@gmail.com>
2023-07-15 01:01:27 +0800

    feat(sd/RateLimiter): tokens bucket

--- sd_RateLimiter/main.go
+++ sd_RateLimiter/main.go
@@ -105,6 +105,8 @@ func NewRateLimiter(strategy RateLimitStrategy) func(c *gin.Context) {
 		return rateLimiter_fixedWindow
 	case "sliding-window":
 		return rateLimiter_slidingWindow
+	case "tokens-bucket":
+		return rateLimiter_tokensBucket
 	default:
 		panic("invalid rate limiting strategy")
 	}
@@ -161,6 +163,84 @@ func rateLimiter_slidingWindow(c *gin.Context) {
 
 	// Automatically clean up old requests from the sliding window by setting an expiry time.
 	_ = redisClient.Expire(key, Window)
+
+	c.Next()
+}
+
+func rateLimiter_tokensBucket(c *gin.Context) {
+	const (
+		rate     = 10  // 10 requests per second
+		capacity = 600 // 10 tokens
+	)
+
+	var now = time.Now().Unix()
+	var requested = 1
+
+	userID := c.GetHeader("x-user-id")
+
+	// Input:
+	// tokensKey (tokens:userID, 123): the number of tokens left
+	// timestampKey (timestamp:userID, 123): the timestamp of the last request
+	// rate: the rate of the bucket in tokens/second
+	// capacity: the capacity of the bucket in tokens
+	// now: the current unix timestamp (seconds since epoch)
+	// requested: the number of tokens requested for the current request
+	// Output:
+	// -1 if the request is rejected, otherwise the number of tokens left before the request is processed
+	luaScript := `
+	local key = KEYS[1]
+	local tokensKey = "tokens:"..key
+	local timestampKey = "timestamp:"..key
+
+	local rate = tonumber(ARGV[1])
+	local capacity = tonumber(ARGV[2])
+	local now = tonumber(ARGV[3])
+	local fill_time = capacity/rate
+	local ttl = math.floor(fill_time*2)
+
+	local tokens = tonumber(redis.call("get", tokensKey))
+	local last_tokens = tokens
+
+	if tokens == nil then
+		tokens = capacity
+		last_tokens = tokens
+	else
+		local last = redis.call("get", timestampKey)
+		if last ~= nil then
+			local elapsed = now - tonumber(last)
+			local fill = elapsed * rate
+			tokens = math.min(capacity, tokens + fill)
+		end
+	end
+
+	local requested = tonumber(ARGV[4])
+	if tokens >= requested then
+		tokens = tokens - requested
+		redis.call("setex", tokensKey, ttl, tokens)
+		redis.call("setex", timestampKey, ttl, now)
+		return last_tokens
+	else
+		return -1
+	end
+`
+
+	sha, err := redisClient.ScriptLoad(luaScript).Result()
+	if err != nil {
+		c.AbortWithError(500, err)
+		return
+	}
+
+	res, err := redisClient.EvalSha(sha, []string{userID}, rate, capacity, now, requested).Result()
+
+	if err != nil {
+		c.AbortWithError(500, err)
+		return
+	}
+
+	if res == -1 {
+		c.AbortWithStatus(429) // 429 Too Many Requests
+		return
+	}
 
 	c.Next()
 }
